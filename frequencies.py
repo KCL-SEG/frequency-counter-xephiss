84daddcc0f7bf79ad6a1e0f8293d761c0df180f9
Xephiss <alex2015lam@gmail.com>
2023-09-26 16:59:16 +0100

    Add code to frequencies method

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,6 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        frequencies[str(item)] = frequencies.get(str(item), 0) + 1
     return frequencies
